969231b29aebbf491c7930a593d79407476522a4
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-04-11 22:20:18 +0000

    Backend funcional

---
+++ server.js
@@ -0,0 +1,42 @@
+// Importações
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ngrok = require("@ngrok/ngrok");
+require('dotenv').config();
+
+// Definições
+const app = express();
+const port = process.env.BACKEND_PORTA; 
+
+// Configuração do CORS para permitir o frontend específico
+const corsOptions = {
+    origin: 'https://dfmrenato.github.io', // Permite requisições apenas deste site
+    methods: 'GET,POST', // Métodos permitidos
+    allowedHeaders: 'Content-Type',
+};
+
+app.use(cors(corsOptions)); // Middleware principal de CORS
+
+// Permite requisições preflight (importante para o navegador validar permissões antes da requisição real)
+app.options('*', cors(corsOptions));
+
+// Middleware para aceitar JSON
+app.use(express.json());
+
+// Conectar ao MongoDB usando o link de conexão fornecido
+const uri = process.env.MONGODB_URI;
+
+// Conectar ao MongoDB
+const client = MongoClient.connect(uri);
+client.then((client) => {
+    db = client.db('comissao');  // Acessa o banco de dados padrão
+    console.log('Conectado ao MongoDB');
+}).catch((error) => {
+    console.error('Erro ao conectar ao MongoDB:', error);
+});
+
+// Rota de exemplo
+app.get('/', (req, res) => {
+    res.send('Olá, mundo! Conectado ao MongoDB!');
+});
