8e70b68b405e6eb6d7818e7e238435ff567351a1
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-04-12 22:32:40 +0000

    Agora vai bb

--- desligado.js
+++ desligado.js
@@ -1,5 +1,8 @@
-function closeIt() {
-    window.location.href = './';
-    return "oi"
-};
-window.onbeforeunload = closeIt;
+window.addEventListener("DOMContentLoaded", () => {
+    const navType = performance.getEntriesByType("navigation")[0]?.type;
+
+    // Só redireciona se o tipo de navegação for 'reload'
+    if (navType === "reload" && window.location.pathname.endsWith("/desligado")) {
+        window.location.href = "./";
+    }
+});
