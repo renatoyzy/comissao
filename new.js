ec89ab83511a407be32c1f0be13c076b2957221a
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-06-13 23:12:49 +0000

    Evitar a desseleção ao editar o input

--- new.js
+++ new.js
@@ -1,7 +1,15 @@
 // Produto selecionável
 Array.from(document.getElementsByClassName('Produto')).forEach(produto => {
-    if(!produto) return;
+    if (!produto) return;
     produto.addEventListener('click', () => {
         produto.classList.toggle('Selecionado');
     });
-})
+
+    // Impede que o clique no input deselect o produto
+    const inputs = produto.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.addEventListener('click', (e) => {
+            e.stopPropagation();
+        });
+    });
+});
