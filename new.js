fbd543fa17d6d5cbfa7e5184bfa09a92d5d84e83
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-06-13 23:16:01 +0000

    Selecionar produto

--- new.js
+++ new.js
@@ -1,15 +1,24 @@
 // Produto selecionável
 Array.from(document.getElementsByClassName('Produto')).forEach(produto => {
+
     if (!produto) return;
+    
+    // Selecionar produto
     produto.addEventListener('click', () => {
         produto.classList.toggle('Selecionado');
     });
 
     // Impede que o clique no input deselect o produto
-    const inputs = produto.querySelectorAll('input');
-    inputs.forEach(input => {
+    produto.querySelectorAll('input').forEach(input => {
         input.addEventListener('click', (e) => {
             e.stopPropagation();
         });
     });
+
+    // Impede que o formulário do produto seja enviado sozinho
+    produto.querySelectorAll('form').forEach(form => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+        })
+    })
 });
