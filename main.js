cf1ff49b7a135b076ab181053c837b592b8e9cf9
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-06-14 00:54:34 +0000

    New -> Principal, Index -> Old

--- main.js
+++ main.js
@@ -1,43 +1,4 @@
-// Vendedor identificado?
-if(!sessionStorage.getItem('vendedor')) {
-    location.href = 'identificar';
-};
-
-// Adicionar estoque no banco de dados
-document.getElementById('FormularioAdicionarEstoque').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    
-    const nome = document.getElementById('FormularioAdicionarEstoque').elements["nome"].value.toLowerCase().replaceAll(' ', '');
-    const valor_da_unidade = parseFloat(document.getElementById('FormularioAdicionarEstoque').elements["valor"].value.replaceAll(',', '.'));
-    const quantidade = document.getElementById('FormularioAdicionarEstoque').elements["quantidade"].value;
-    const data_criacao = new Date();
-
-    try {
-        // Comunicação com o backend
-        const response = await fetch('https://evolved-legible-spider.ngrok-free.app/adicionar-estoque', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ nome, quantidade, data_criacao, valor_da_unidade })
-        });
-
-        const data = await response.json();
-        
-        if(data.error_message) return alert(`Erro de comunicação\n${data.error_message}`);
-
-        if (!response.ok) {
-            throw new Error('Falha na solicitação');
-        }
-
-        location.reload();
-        
-    } catch (error) {
-        console.error(error);
-        alert(`Erro ao tentar comunicação\n${error}`);
-    }
-});
-
+// Adicionar produtos
 // Escrever estoque e fiados pro usuário
 (async () => {
     try {
@@ -62,59 +23,61 @@ document.getElementById('FormularioAdicionarEstoque').addEventListener('submit',
         }
 
         (() => {
-
-            document.getElementById('RegistrarVendaProduto').innerHTML = '';
-            let produtos_string = [];
         
             data.produtos.sort((a, b) => a.nome.localeCompare(b.nome)).forEach(produto => {
-                produtos_string.push(`${produto.nome} ${produto.quantidade} (R$${produto.valor_da_unidade})`);
 
-                document.getElementById('RegistrarVendaProduto').innerHTML += `
-                    <option value="${produto.nome}">${produto.nome}</option>
+                document.getElementById("Produtos").innerHTML += `
+                    <div class="Produto">
+                        <img src="https://snowfruit.com.br/wp-content/webp-express/webp-images/uploads/2021/12/Picole-1.png.webp">
+                        <label id="${produto.nome.replace(
+                            /\w\S*/g,
+                            text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+                        )}">
+                            <i class="fa-solid fa-circle-check"></i>
+                            ${produto.nome}
+                            <form>
+                                <input type="number" name="quantidade" id="quantidade" value="1">
+                            </form>
+                        </label>
+                    </div>
                 `;
+                
+                // Produto selecionável
+                Array.from(document.getElementsByClassName('Produto')).forEach(produto => {
+
+                    if (!produto) return;
+                    
+                    // Selecionar produto
+                    produto.addEventListener('click', () => {
+                        produto.classList.toggle('Selecionado');
+
+                        // Ativar aside caso tenha produtos selecionados
+                        if(document.querySelectorAll('.Produto.Selecionado').length<1) {
+                            document.querySelector('aside').classList.remove('Ativo');
+                        } else {
+                            document.querySelector('aside').classList.add('Ativo');
+                        };
+                    });
+
+                    // Impede que o clique no input deselect o produto
+                    produto.querySelectorAll('input').forEach(input => {
+                        input.addEventListener('click', (e) => {
+                            e.stopPropagation();
+                        });
+                    });
+
+                    // Impede que o formulário do produto seja enviado sozinho
+                    produto.querySelectorAll('form').forEach(form => {
+                        form.addEventListener('submit', (e) => {
+                            e.preventDefault();
+                        })
+                    })
+
+                });
+
+                //produtos_string.push(`${produto.nome} ${produto.quantidade} (R$${produto.valor_da_unidade})`);
+
             });
-
-            produtos_string = produtos_string.join('<br>');
-
-            document.getElementById("CampoDadosDb").innerHTML = produtos_string;
-
-        })();
-
-        // Escrever fiados
-
-        // Comunicação com o backend
-        response = await fetch('https://evolved-legible-spider.ngrok-free.app/obter-devedores', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ })
-        });
-
-        data = await response.json();
-        
-        if(data.error_message) return alert(`Erro de comunicação\n${data.error_message}`);
-
-        if (!response.ok) {
-            throw new Error('Falha na solicitação');
-        }
-
-        (() => {
-
-            document.getElementById('NomeDevedorPagando').innerHTML = '';
-            let devedores_string = [];
-        
-            data.devedores.sort((a, b) => a.nome.localeCompare(b.nome)).forEach(devedor => {
-                devedores_string.push(`${devedor.nome}: R$${devedor.divida}`);
-
-                document.getElementById('NomeDevedorPagando').innerHTML += `
-                    <option value="${devedor.nome}">${devedor.nome}</option>
-                `;
-            });
-
-            devedores_string = devedores_string.join('<br>');
-
-            document.getElementById("CampoDevedoresDb").innerHTML = devedores_string;
 
         })();
         
@@ -127,89 +90,3 @@ document.getElementById('FormularioAdicionarEstoque').addEventListener('submit',
         };
     }
 })();
-
-// Registrar venda no banco de dados
-document.getElementById('FormularioRegistrarVenda').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    
-    let nome_pre;
-    if(!document.getElementById('FormularioRegistrarVenda').elements["nome"].value) {
-        nome_pre = "NÃO INFORMADO";
-    } else {
-        nome_pre = document.getElementById('FormularioRegistrarVenda').elements["nome"].value.toUpperCase();
-    };
-    const nome = nome_pre;
-    const produto = document.getElementById('FormularioRegistrarVenda').elements["produto"].value.toLowerCase();
-    const quantidade = parseInt(document.getElementById('FormularioRegistrarVenda').elements["quantidade"].value);
-    const valor = parseFloat(document.getElementById('FormularioRegistrarVenda').elements["valor"].value.replaceAll(",","."));
-    const metodo_de_pagamento = document.getElementById('FormularioRegistrarVenda').elements["metodo_de_pagamento"].value.toUpperCase();
-    const fiado = document.getElementById('FormularioRegistrarVenda').elements["fiado"].value.toUpperCase();
-    const vendedor = sessionStorage.getItem('vendedor');
-    const data_venda = new Intl.DateTimeFormat('pt-BR', {
-        timeZone: 'America/Sao_Paulo',
-        year: '2-digit',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-    }).format(new Date()).replace(',', '');
-
-    try {
-        // Comunicação com o backend
-        const response = await fetch('https://evolved-legible-spider.ngrok-free.app/registrar-venda', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ nome, produto, quantidade, valor, metodo_de_pagamento, fiado, vendedor, data_venda })
-        });
-
-        const data = await response.json();
-        
-        if(data.error_message) return alert(`Erro de comunicação\n${data.error_message}`);
-
-        if (!response.ok) {
-            throw new Error('Falha na solicitação');
-        }
-
-        location.reload();
-        
-    } catch (error) {
-        console.error(error);
-        alert(`Erro ao tentar comunicação\n${error}`);
-    };
-});
-
-// Reduzir dívida no banco de dados
-document.getElementById('FormularioPagarDivida').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    
-    const devedor = document.getElementById('FormularioPagarDivida').elements["devedor"].value.toUpperCase();
-    const valor = parseFloat(document.getElementById('FormularioPagarDivida').elements["valor"].value.replaceAll(",","."));
-
-    try {
-        // Comunicação com o backend
-        const response = await fetch('https://evolved-legible-spider.ngrok-free.app/pagar-divida', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ devedor, valor })
-        });
-
-        const data = await response.json();
-        
-        if(data.error_message) return alert(`Erro de comunicação\n${data.error_message}`);
-
-        if (!response.ok) {
-            throw new Error('Falha na solicitação');
-        }
-
-        location.reload();
-        
-    } catch (error) {
-        console.error(error);
-        alert(`Erro ao tentar comunicação\n${error}`);
-    }
-});
