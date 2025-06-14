a6ab63a49ca31686e8b7e246ca77d1932b8beb3b
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-06-14 00:10:41 +0000

    AAAAAAAAA

--- new.js
+++ new.js
@@ -41,6 +41,39 @@
                         </label>
                     </div>
                 `;
+                
+                // Produto selecionável
+                Array.from(document.getElementsByClassName('Produto')).forEach(produto => {
+
+                    if (!produto) return;
+                    
+                    // Selecionar produto
+                    produto.addEventListener('click', () => {
+                        produto.classList.toggle('Selecionado');
+
+                        // Ativar aside caso tenha produtos selecionados
+                        if(document.querySelectorAll('.Produto.Selecionado').length<1) {
+                            document.querySelector('aside').classList.remove('Ativo');
+                        } else {
+                            document.querySelector('aside').classList.add('Ativo');
+                        };
+                    });
+
+                    // Impede que o clique no input deselect o produto
+                    produto.querySelectorAll('input').forEach(input => {
+                        input.addEventListener('click', (e) => {
+                            e.stopPropagation();
+                        });
+                    });
+
+                    // Impede que o formulário do produto seja enviado sozinho
+                    produto.querySelectorAll('form').forEach(form => {
+                        form.addEventListener('submit', (e) => {
+                            e.preventDefault();
+                        })
+                    })
+
+                });
 
                 //produtos_string.push(`${produto.nome} ${produto.quantidade} (R$${produto.valor_da_unidade})`);
 
@@ -57,36 +90,3 @@
         };
     }
 })();
-
-// Produto selecionável
-Array.from(document.getElementsByClassName('Produto')).forEach(produto => {
-
-    if (!produto) return;
-    
-    // Selecionar produto
-    produto.addEventListener('click', () => {
-        produto.classList.toggle('Selecionado');
-
-        // Ativar aside caso tenha produtos selecionados
-        if(document.querySelectorAll('.Produto.Selecionado').length<1) {
-            document.querySelector('aside').classList.remove('Ativo');
-        } else {
-            document.querySelector('aside').classList.add('Ativo');
-        };
-    });
-
-    // Impede que o clique no input deselect o produto
-    produto.querySelectorAll('input').forEach(input => {
-        input.addEventListener('click', (e) => {
-            e.stopPropagation();
-        });
-    });
-
-    // Impede que o formulário do produto seja enviado sozinho
-    produto.querySelectorAll('form').forEach(form => {
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-        })
-    })
-
-});
