5a46cfd8be9014473985d05c1c41171e8743fb77
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-06-14 02:38:50 +0000

    On Input

--- index.js
+++ index.js
@@ -84,18 +84,22 @@
 })();
 
 // Atualizar valor total
-document.addEventListener('click', () => {
-
-    if (!document.querySelector('aside').classList.contains('Ativo')) return;
+document.querySelectorAll('input#quantidade').forEach(input => {
     
-    let dados_volateis = '';
-    let valor_total = 0;
-    document.querySelectorAll('.Produto.Selecionado').forEach(produto => {
-        dados_volateis += `- ${produto.querySelector('label').id} (${produto.querySelector('input#quantidade').valueAsNumber}xR$${produto.querySelector('input#valor_da_unidade').value})<br>`;
-        valor_total += parseInt(produto.querySelector('input#valor_da_unidade').value)*parseInt(produto.querySelector('input#quantidade').value);
-    });
+    input.addEventListener('input', () => {
 
-    document.querySelector('aside').querySelector('#DadosVolateis').innerHTML = dados_volateis+`<h3>TOTAL: R$${valor_total}</h3>`;
+        if (!document.querySelector('aside').classList.contains('Ativo')) return;
+
+        let dados_volateis = '';
+        let valor_total = 0;
+        document.querySelectorAll('.Produto.Selecionado').forEach(produto => {
+            dados_volateis += `- ${produto.querySelector('label').id} (${produto.querySelector('input#quantidade').valueAsNumber}xR$${produto.querySelector('input#valor_da_unidade').value})<br>`;
+            valor_total += parseInt(produto.querySelector('input#valor_da_unidade').value)*parseInt(produto.querySelector('input#quantidade').value);
+        });
+
+        document.querySelector('aside').querySelector('#DadosVolateis').innerHTML = dados_volateis+`<h3>TOTAL: R$${valor_total}</h3>`;
+        
+    });
 });
 
 // Registrar venda no banco de dados
