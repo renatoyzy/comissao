d6649602ad006399755477bef4db2157eb6802cc
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-06-14 16:49:31 +0000

    Correção de erros

--- estoque.js
+++ estoque.js
@@ -28,7 +28,6 @@ if(!sessionStorage.getItem('vendedor')) {
 
         (() => {
 
-            document.getElementById('RegistrarVendaProduto').innerHTML = '';
             let produtos_string = [];
         
             data.produtos.sort((a, b) => a.nome.localeCompare(b.nome)).forEach(produto => {
@@ -88,41 +87,6 @@ if(!sessionStorage.getItem('vendedor')) {
         };
     }
 })();
-
-// Adicionar estoque no banco de dados
-document.getElementById('FormularioAdicionarEstoque').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    
-    const nome = document.getElementById('FormularioAdicionarEstoque').elements["nome"].value.toLowerCase().replaceAll(' ', '');
-    const valor_da_unidade = parseFloat(document.getElementById('FormularioAdicionarEstoque').elements["valor"].value.replaceAll(',', '.'));
-    const quantidade = document.getElementById('FormularioAdicionarEstoque').elements["quantidade"].value;
-    const data_criacao = new Date();
-
-    try {
-        // Comunicação com o backend
-        const response = await fetch('https://evolved-legible-spider.ngrok-free.app/adicionar-estoque', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ nome, quantidade, data_criacao, valor_da_unidade })
-        });
-
-        const data = await response.json();
-        
-        if(data.error_message) return alert(`Erro de comunicação\n${data.error_message}`);
-
-        if (!response.ok) {
-            throw new Error('Falha na solicitação');
-        }
-
-        location.reload();
-        
-    } catch (error) {
-        console.error(error);
-        alert(`Erro ao tentar comunicação\n${error}`);
-    }
-});
 
 // Reduzir dívida no banco de dados
 document.getElementById('FormularioPagarDivida').addEventListener('submit', async (event) => {
--- novoproduto.js
+++ novoproduto.js
@@ -2,3 +2,38 @@
 if(!sessionStorage.getItem('vendedor')) {
     location.href = 'identificar';
 };
+
+// Adicionar estoque no banco de dados
+document.getElementById('FormularioAdicionarEstoque').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const nome = document.getElementById('FormularioAdicionarEstoque').elements["nome"].value.toLowerCase().replaceAll(' ', '');
+    const valor_da_unidade = parseFloat(document.getElementById('FormularioAdicionarEstoque').elements["valor"].value.replaceAll(',', '.'));
+    const quantidade = document.getElementById('FormularioAdicionarEstoque').elements["quantidade"].value;
+    const data_criacao = new Date();
+
+    try {
+        // Comunicação com o backend
+        const response = await fetch('https://evolved-legible-spider.ngrok-free.app/adicionar-estoque', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ nome, quantidade, data_criacao, valor_da_unidade })
+        });
+
+        const data = await response.json();
+        
+        if(data.error_message) return alert(`Erro de comunicação\n${data.error_message}`);
+
+        if (!response.ok) {
+            throw new Error('Falha na solicitação');
+        }
+
+        location.reload();
+        
+    } catch (error) {
+        console.error(error);
+        alert(`Erro ao tentar comunicação\n${error}`);
+    }
+});
