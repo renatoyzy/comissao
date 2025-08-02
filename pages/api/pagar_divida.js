bc91883da44fbb6fac1672fd60264dc467ca4b1e
Renato <renato.augusto.almeida.santos@gmail.com>
2025-08-02 01:04:43 +0000

    Registering vendas fixes

--- pages/api/pagar_divida.js
+++ pages/api/pagar_divida.js
@@ -29,11 +29,15 @@ export default async function handler(req, res) {
 
     if (!devedor_achado) return res.status(404).json({ error_message: 'Devedor não encontrado no banco.' });
 
-    await client.db('comissao').collection('devedores').deleteOne({ nome: devedor_achado.nome });
-
     if (devedor_achado.divida - valor > 0) {
-      await client.db('comissao').collection('devedores').insertOne({ nome: devedor_achado.nome, divida: devedor_achado.divida - valor });
-    };
+      await client.db('comissao').collection('devedores').findOneAndUpdate(
+        { nome: devedor_achado.nome },
+        { $inc: { divida: -valor } },
+        { returnDocument: "after", upsert: true }
+      );
+    } else {
+      await client.db('comissao').collection('devedores').deleteOne({ nome: devedor_achado.nome });
+    }
 
     return res.status(201).json({ certo: true });
 
